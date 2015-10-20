bf1dc685aa54052c7828faa46c90a6c6952afddc
Kamal Marhubi <kamal@marhubi.com>
2015-10-20 17:27:47 -0400

    Add new game button to home page

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,6 +1,11 @@
-from flask import abort
-from flask import render_template
+import uuid
 
+from flask import abort
+from flask import redirect
+from flask import render_template
+from flask import url_for
+
+from game import Game
 import store
 
 from tictactoe import app
@@ -11,6 +16,17 @@ game_store = store.PickledDictGameStore('/tmp/tictactoe')
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/game", methods=['POST'])
+def new_game():
+    game_id = uuid.uuid4().hex
+    game = Game()
+
+    game_store.put(game_id, game)
+
+    return redirect(url_for('game', game_id=game_id))
+
 
 @app.route("/game/<game_id>")
 def game(game_id):
