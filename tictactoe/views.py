4e179bc454616aca830999ec97d2a6f118aedb70
Kamal Marhubi <kamal@marhubi.com>
2015-10-20 17:49:40 -0400

    Allow making moves

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -3,6 +3,7 @@ import uuid
 from flask import abort
 from flask import redirect
 from flask import render_template
+from flask import request
 from flask import url_for
 
 from game import Game
@@ -35,4 +36,22 @@ def game(game_id):
     if game is None:
         abort(404)  # not found
 
-    return render_template('game.html', game=game)
+    return render_template('game.html', game=game, game_id=game_id)
+
+
+@app.route("/game/<game_id>/move", methods=['POST'])
+def make_move(game_id):
+    game = game_store.get(game_id)
+
+    if game is None:
+        abort(404)  # not found
+
+    move = request.form.get('move', None)
+
+    player, x, y = move.split('|')  # TODO: validate
+
+    game.make_move(player.lower(), (int(x), int(y)))
+
+    game_store.put(game_id, game)
+
+    return redirect(url_for('game', game_id=game_id))
