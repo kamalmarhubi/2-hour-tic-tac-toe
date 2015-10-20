bb0c1577c5087b3974c7a5a20d52cb1839e379ce
Kamal Marhubi <kamal@marhubi.com>
2015-10-20 16:50:46 -0400

    Add pickled dict game store

--- tictactoe/store.py
+++ tictactoe/store.py
@@ -1,4 +1,6 @@
 import abc
+import pickle
+
 
 class GameStore(object):
     __metaclass__ = abc.ABCMeta
@@ -19,6 +21,32 @@ class DictGameStore(GameStore):
 
     def put(self, key, game):
         self.games[key] = game
+
+    def get(self, key):
+        return self.games.get(key, None)
+
+
+class PickledDictGameStore(GameStore):
+
+    def __init__(self, filename):
+        self.filename = filename
+
+        try:
+            with open(self.filename) as f:
+                self.games = pickle.load(f)
+        except IOError as e:
+            if e.errno != 2:  # 2 is ENOENT, ie file doesn't exist
+                raise
+
+            self.games = dict()
+            with open(self.filename, 'w') as f:
+                pickle.dump(self.games, f)
+
+    def put(self, key, game):
+        self.games[key] = game
+
+        with open(self.filename, 'w') as f:
+            pickle.dump(self.games, f)
 
     def get(self, key):
         return self.games.get(key, None)
