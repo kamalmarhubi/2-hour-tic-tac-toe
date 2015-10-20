25b22d2de5d71ba8d7df35206135f4874853d70e
Kamal Marhubi <kamal@marhubi.com>
2015-10-20 16:56:50 -0400

    Look up game in store

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,10 +1,22 @@
+from flask import abort
 from flask import render_template
 
+import store
+
 from tictactoe import app
+
+# TODO: use app.config for this
+game_store = store.PickledDictGameStore('/tmp/tictactoe')
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/game/<game_id>"):
+@app.route("/game/<game_id>")
+def game(game_id):
+    game = game_store.get(game_id)
+
+    if game is None:
+        abort(404)  # not found
+
     return render_template('game.html')
